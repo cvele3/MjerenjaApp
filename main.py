60e0b2e4ac19a0d111e1dfe42d912fa1a3cdc911
cvele3 <cvetkojakov@gmail.com>
2023-02-11 15:53:01 +0100

    plotting graphs

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
 import sys
-import measureTemperature
+import random
+import time
+#import measureTemperature
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QTextEdit, QComboBox
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+class LivePlot(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        self.axes.set_xlabel('Time (s)')
+        self.axes.set_ylabel('Temperature (°C)')
+        self.axes.set_ylim(20, 35)
+        self.line, = self.axes.plot([], [], '-')
+        super().__init__(fig)
+        self.setParent(parent)
+
+        self.temperature_func = None
+        self.anim = None
+
+    def update_plot(self, x, y):
+        x, y = zip(*sorted(zip(x, y)))  # add this line to sort x and y in ascending order
+        self.line.set_data(x, y)
+        self.axes.relim()
+        self.axes.autoscale_view(True, True, True)
+        self.axes.set_xlim(0, x[-1])  # add this line to set x-axis limits
+        self.draw()
+
+    def start_animation(self, temperature_func):
+        self.temperature_func = temperature_func
+        self.anim = FuncAnimation(self.figure, self._update_animation, interval=100)
+
+    def _update_animation(self, frame):
+        xdata, ydata = self.line.get_data()
+        xdata = np.append(xdata, frame)
+        ydata = np.append(ydata, self.temperature_func())
+        self.update_plot(xdata, ydata)
+
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -17,14 +58,14 @@ class MainWindow(QWidget):
         # Create the Termopar options menu
         self.Termopar_options = QComboBox()
         self.Termopar_options.addItem("Select Termopar type")
-        self.Termopar_options.addItem("T  ->  0 - 350")
-        self.Termopar_options.addItem("J  ->  95 - 760")
-        self.Termopar_options.addItem("E  ->  95 - 900")
-        self.Termopar_options.addItem("K  ->  95 - 1260")
-        self.Termopar_options.addItem("N  ->  95 - 1260")
-        self.Termopar_options.addItem("R  ->  870 - 1450")
-        self.Termopar_options.addItem("S  ->  980 - 1450")
-        self.Termopar_options.addItem("B  ->  871 - 1704")
+        # self.Termopar_options.addItem("T  ->  0 - 350")
+        # self.Termopar_options.addItem("J  ->  95 - 760")
+        # self.Termopar_options.addItem("E  ->  95 - 900")
+        # self.Termopar_options.addItem("K  ->  95 - 1260")
+        # self.Termopar_options.addItem("N  ->  95 - 1260")
+        # self.Termopar_options.addItem("R  ->  870 - 1450")
+        # self.Termopar_options.addItem("S  ->  980 - 1450")
+        # self.Termopar_options.addItem("B  ->  871 - 1704")
 
         # Create the text boxes
         self.PT100_text = QTextEdit()
@@ -39,30 +80,29 @@ class MainWindow(QWidget):
         # Create the layout
         layout = QVBoxLayout()
         layout.addWidget(self.PT100_button)
-        layout.addWidget(self.PT100_text)
+        # layout.addWidget(self.PT100_text)
         layout.addWidget(self.Termopar_button)
         layout.addWidget(self.Termopar_options)
-        layout.addWidget(self.Termopar_text)
+        # layout.addWidget(self.Termopar_text)
         layout.addWidget(self.Both_button)
         layout.addWidget(self.Termopar_options)
-        layout.addWidget(self.Both_text_1)
-        layout.addWidget(self.Both_text_2)
+        # layout.addWidget(self.Both_text_1)
+        # layout.addWidget(self.Both_text_2)
 
         self.setLayout(layout)
 
     def PT100_clicked(self):
-        temperaturePT = measureTemperature.pt100_temperature()
-        self.PT100_text.setPlainText("PT100 temperature: " + str(temperaturePT))
+        live_plot = LivePlot()
+        live_plot.start_animation(lambda: random.uniform(25, 30))
+        live_plot.show()
 
     def Termopar_clicked(self):
-        temperatureTC = measureTemperature.termopar_temperature()
-        self.Termopar_text.setPlainText("ThermoCouple temperature: " + str(temperatureTC))
+        live_plot = LivePlot()
+        live_plot.start_animation(lambda: random.uniform(25, 30))
+        live_plot.show()
 
     def Both_clicked(self):
-        temperaturePT = measureTemperature.pt100_temperature()
-        temperatureTC = measureTemperature.termopar_temperature()
-        self.PT100_text.setPlainText("PT100 temperature: " + str(temperaturePT))
-        self.Termopar_text.setPlainText("ThermoCouple temperature: " + str(temperatureTC))
+        #treba napravit da se prikazuju 2 grafa u istom prozoru ili kako god, sam da budu dva grafa
 
 
 if __name__ == '__main__':
